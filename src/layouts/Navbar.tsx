b5959f5d0a17bd3c2cfc0258f7611a37ae56b71d
Purna Shrestha <crush9918@gmail.com>
2024-12-11 19:19:39 +0545

    feat: UPdate navigation

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../assets/logo.png';
 import { TbMenu } from 'react-icons/tb';
-import { MdOutlineCall } from 'react-icons/md';
-import { Link, matchPath, NavLink, useLocation } from 'react-router-dom';
+import { matchPath, NavLink, useLocation } from 'react-router-dom';
 import blueGradient from '../assets/pattern/blue-gradient.png';
 
 const navLinks = [
@@ -18,29 +17,45 @@ const navLinks = [
     title: 'About',
     url: '/about',
     demoImage:
-      'https://hotelmoonlight.com/images/gallery/galleryimages/b4EyZ-r7.jpg',
+      'https://hotelmoonlight.com/images/gallery/galleryimages/JfNu4-e1.jpg',
     priority: 'low',
   },
   {
     id: 3,
-    title: 'Accommodation',
-    url: '/accommodation',
-    demoImage: 'https://hotelmoonlight.com/images/slideshow/Nk7Bq-slide1.jpg',
+    title: 'Stay',
+    url: '/stay',
+    demoImage: 'https://hotelmoonlight.com/images/gallery/galleryimages/c0A4o-r1.jpg',
     priority: 'high',
   },
   {
     id: 4,
-    title: 'Services',
-    url: '/services',
+    title: 'Fine Dine',
+    url: '/dine',
     demoImage:
       'https://hotelmoonlight.com/images/gallery/galleryimages/b4EyZ-r7.jpg',
     priority: 'high',
   },
   {
     id: 5,
+    title: 'Occasions',
+    url: '/occasions',
+    demoImage:
+      'https://hotelmoonlight.com/images/gallery/galleryimages/5KZxo-h5.jpg',
+    priority: 'high',
+  },
+  {
+    id: 6,
+    title: 'SPA',
+    url: '/spa',
+    demoImage:
+      'https://hotelmoonlight.com/assets/userfiles/images/spa2.jpg',
+    priority: 'high',
+  },
+  {
+    id: 5,
     title: 'Contact us',
     url: '/contact',
-    demoImage: 'https://hotelmoonlight.com/images/slideshow/Nk7Bq-slide1.jpg',
+    demoImage: 'https://hotelmoonlight.com/images/gallery/galleryimages/sfcI4-e4.jpg',
     priority: 'low',
   },
   {
@@ -48,7 +63,21 @@ const navLinks = [
     title: 'Promotions',
     url: '/offers',
     demoImage:
-      'https://hotelmoonlight.com/images/gallery/galleryimages/b4EyZ-r7.jpg',
+      'https://hotelmoonlight.com/images/gallery/galleryimages/fVO9e-r14.jpg',
+    priority: 'low',
+  },
+  {
+    id: 7,
+    title: 'Gallery',
+    url: '/gallery',
+    demoImage: 'https://hotelmoonlight.com/images/gallery/galleryimages/6hgTT-r11.jpg',
+    priority: 'low',
+  },
+  {
+    id: 8,
+    title: 'CSR',
+    url: '/csr',
+    demoImage: 'https://hotelmoonlight.com/images/gallery/galleryimages/1PXk5-r8.jpg',
     priority: 'low',
   },
 ];
@@ -81,6 +110,10 @@ const Navbar: React.FC = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [prevScrollPos]);
 
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location]);
+
   const toggleNav = () => {
     setIsOpen(!isOpen);
   };
@@ -91,102 +124,47 @@ const Navbar: React.FC = () => {
   return (
     <>
       <header
-        className={`fixed top-0 z-40 w-full transition-all duration-[1s] ${
-          window.scrollY > 0 ? '' : ''
-        } ${visible ? '' : '-translate-y-full'}`}
+        className={`pointer-events-none fixed top-0 z-[60] flex w-full items-start justify-between p-6 transition-all duration-[1s]`}
       >
-        <div
-          className={`z-40 flex items-start justify-between px-4 py-6 md:px-8 ${
-            visible
-              ? 'bg-gradient-to-t from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0)]'
-              : 'bg-transparent'
-          }${window.scrollY > 0 ? 'flex items-center justify-between' : ''} `}
+        <a
+          href="/"
+          className={`pointer-events-auto origin-top object-contain transition-all duration-[1s] ${
+            visible ? '-translate-y-0' : '-translate-y-40 scale-50'
+          }`}
         >
-          <button
-            className="rounded-0 relative flex flex-col items-center justify-center text-light outline outline-1 outline-white/30 transition-all duration-700 ease-linear"
-            onClick={toggleNav}
-          >
-            <span className="m-2 mb-0 bg-blue-800 px-2 font-light">Menu</span>
-            <TbMenu className="scale-x-[3] text-2xl" />
-            {/* <svg
-                stroke="currentColor"
-                fill="currentColor"
-                stroke-width="0"
-                viewBox="0 0 512 512"
-                className="absolute left-0 top-1/2 -translate-y-1/2 translate-x-4 scale-75 text-xl opacity-0 transition-all duration-700 ease-linear"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M331.3 308.7L278.6 256l52.7-52.7c6.2-6.2 6.2-16.4 0-22.6-6.2-6.2-16.4-6.2-22.6 0L256 233.4l-52.7-52.7c-6.2-6.2-15.6-7.1-22.6 0-7.1 7.1-6 16.6 0 22.6l52.7 52.7-52.7 52.7c-6.7 6.7-6.4 16.3 0 22.6 6.4 6.4 16.4 6.2 22.6 0l52.7-52.7 52.7 52.7c6.2 6.2 16.4 6.2 22.6 0 6.3-6.2 6.3-16.4 0-22.6z"></path>
-                <path d="M256 76c48.1 0 93.3 18.7 127.3 52.7S436 207.9 436 256s-18.7 93.3-52.7 127.3S304.1 436 256 436c-48.1 0-93.3-18.7-127.3-52.7S76 304.1 76 256s18.7-93.3 52.7-127.3S207.9 76 256 76m0-28C141.1 48 48 141.1 48 256s93.1 208 208 208 208-93.1 208-208S370.9 48 256 48z"></path>
-              </svg>
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                stroke-width="0"
-                viewBox="0 0 24 24"
-                className="absolute left-0 top-1/2 -translate-y-1/2 translate-x-4 scale-100 text-xl opacity-100 transition-all duration-700 ease-linear"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M3 4H21V6H3V4ZM3 11H15V13H3V11ZM3 18H21V20H3V18Z"></path>
-              </svg> */}
-          </button>
+          <img
+            src={logo}
+            alt="logo"
+            className={`filter-white h-28 w-40 object-contain`}
+          />
+        </a>
+        <button
+          className={`group pointer-events-auto relative flex flex-col items-center justify-center transition-all duration-700 ease-linear ${
+            window.scrollY > 0
+              ? 'bg-white text-dark'
+              : 'bg-transparent text-white'
+          }`}
+          onClick={toggleNav}
+        >
+          <span className="px-2 text-xs font-light uppercase tracking-wide group-hover:animate-pulse">
+            Menu
+          </span>
+          <TbMenu className="-mt-1 origin-center scale-x-[2] text-2xl group-hover:animate-spin" />
+        </button>
 
-          <h1 className="absolute left-1/2 -translate-x-1/2">
-            <a
-              href="/"
-              className={`object-contain transition-all duration-[1s] ${
-                visible ? '' : ''
-              } ${window.scrollY > 0 ? '' : ''}`}
-            >
-              {/* <img
-                src="https://hotelhimalaya.pages.dev/assets/logo-s4LMycLK.svg"
-                alt="logo"
-                className={`object-contain transition-all duration-[1s]  ${
-                  visible
-                    ? "w-40 h-36 object-contain -translate-y-0"
-                    : "scale-0 -translate-y-6 bg-transparent"
-                } ${
-                  window.scrollY > 0
-                    ? "w-36 h-12 md:scale-75 p-4 translate-y-0 mt-0"
-                    : // ? "filter-black w-16 h-12 object-contain scale-150 -mt-[4px] p-2"
-                      ""
-                }`}
-              /> */}
-              <img
-                src={logo}
-                alt="logo"
-                className={`filter-white -mt-6 h-36 w-40 object-contain transition-all duration-[1s] ${
-                  visible ? '-translate-y-0' : '-translate-y-6 scale-0'
-                } ${
-                  window.scrollY > 0
-                    ? '-translate-y-6 scale-0'
-                    : // "filter-white w-16 h-12 object-contain scale-150 -mt-[4px] p-2"
-                      ''
-                }`}
-              />
-            </a>
-          </h1>
-
-          <button
+        {/* <button
             className={`visible relative flex flex-col items-center justify-center gap-2 p-2 text-light outline outline-1 outline-white/30 transition-all duration-700 ease-linear`}
             title="Menu"
             aria-label="Menu"
           >
             <span className="w-full bg-blue-800 font-light">Book Now</span>
-            {/* <TbMenu className="scale-x-[3] text-2xl" />
-             */}
             <span className="flex items-center gap-1 text-xs font-light text-light">
               or <MdOutlineCall />
               <Link to="tel:+977-1-4980452" className="text-light">
                 +977-1-4980452
               </Link>
             </span>
-          </button>
-        </div>
+          </button> */}
       </header>
 
       <nav
@@ -201,15 +179,6 @@ const Navbar: React.FC = () => {
           style={{ backgroundImage: `url(${blueGradient})` }}
           aria-hidden="true"
         />
-        <button
-          onClick={() => setIsOpen(false)}
-          className="absolute left-10 top-6 flex items-center justify-center gap-2 py-0 px-3 text-4xl text-light outline outline-1 outline-white/30 transition-all duration-700 ease-linear"
-          aria-label="Close"
-          title="Close"
-        >
-          &times;
-        </button>
-
         <div className="container grid size-full grid-cols-2 content-center items-center gap-12">
           {/* Dynamic Image */}
           <div className="relative flex size-full items-end justify-end">
@@ -221,7 +190,7 @@ const Navbar: React.FC = () => {
           </div>
 
           {/* Navigation Links */}
-          <aside className="space-y-24">
+          <aside className="space-y-16">
             {/* High Priority Links */}
             <ul className="links flex flex-col items-start justify-start gap-2 md:gap-8">
               {highPriorityLinks.map((link) => (
@@ -238,7 +207,7 @@ const Navbar: React.FC = () => {
                     aria-label={link.title}
                   >
                     <span
-                      className={`h-px transition-all ${
+                      className={`transition-700 h-px ${
                         activeLink?.id === link.id ? 'w-16' : 'w-0'
                       } bg-light group-hover:w-16`}
                     ></span>
@@ -249,7 +218,7 @@ const Navbar: React.FC = () => {
             </ul>
 
             {/* Low Priority Links */}
-            <ul className="links flex max-w-sm flex-wrap gap-2 md:gap-8">
+            <ul className="links flex max-w-sm flex-wrap gap-2 md:gap-5">
               {lowPriorityLinks.map((link) => (
                 <li key={link.id} className="group">
                   <NavLink
