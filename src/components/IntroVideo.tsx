55d5c03dbfb5eb564b31277394684bd9a1ae502b
Purna Shrestha <crush9918@gmail.com>
2025-01-10 13:33:15 +0545

    temp: Add temp design for the intro video

--- src/components/IntroVideo.tsx
+++ src/components/IntroVideo.tsx
@@ -12,8 +12,8 @@ const IntroVideo: React.FC = () => {
   };
 
   return (
-    <>
-      <div className="mt-16 md:mt-0 responsive-banner relative top-0 h-full w-full overflow-hidden lg:h-screen">
+    <main className="relative m-0 p-0">
+      <div className="responsive-banner relative top-0 mt-16 h-full w-full overflow-hidden md:mt-0 lg:h-screen">
         {/* <div className="overlay absolute top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.25)]"></div> */}
         <video
           ref={videoRef}
@@ -35,7 +35,49 @@ const IntroVideo: React.FC = () => {
           A video of Moonlight.
         </video>
       </div>
-    </>
+      {/* <div className="absolute inset-0 inline-flex flex-col items-center text-light justify-end gap-8 bg-gradient-to-t from-black/50 to-transparent pb-20">
+        <h2 className="text-shadow-glow text-center text-9xl">
+          Hotel Moonlight
+        </h2>
+        <ul className="flex items-center justify-center gap-5 text-2xl">
+          <li>
+            <a
+              href="#about"
+              className="font-title text-2xl uppercase"
+            >
+              Stay
+            </a>
+          </li>
+          •
+          <li>
+            <a
+              href="#gallery"
+              className="font-title text-2xl uppercase"
+            >
+              Dine
+            </a>
+          </li>
+          •
+          <li>
+            <a
+              href="#contact"
+              className="font-title text-2xl uppercase"
+            >
+              Wellness
+            </a>
+          </li>
+          •
+          <li>
+            <a
+              href="#contact"
+              className="font-title text-2xl uppercase"
+            >
+              Occasion
+            </a>
+          </li>
+        </ul>
+      </div> */}
+    </main>
   );
 };
 
